d8611316e758558017f7e45d1a2287189579ed06
pradhanpobitra <pobitra.pradhan.pp@gmail.com>
2021-02-02 23:41:39 +0530

    second version

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, {useState, useEffect} from 'react'
 import alanBtn from '@alan-ai/alan-sdk-web'
 import NewsCards from './components/NewsCards/NewsCards'
-
+import useStyles from './styles'
 
 const alanKey = '2828f5b33df0ce5169d3d72640c338f72e956eca572e1d8b807a3e2338fdd0dc/stage'
 
+const imgSrc = 'https://media-exp1.licdn.com/dms/image/C561BAQFzAiAvq0Jg8Q/company-background_10000/0/1565260089604?e=2159024400&v=beta&t=ygcEIMzRHClwTjBwChX2naoGiS2TCeDwozFfEAM73ek'
 const App = () => {
     const [newsArticles , setNewsArticles] = useState([])
 
+    const classes = useStyles()
     useEffect(() => {
         alanBtn({
             key: alanKey,
@@ -20,7 +22,9 @@ const App = () => {
     },[])
     return (
         <div>
-            <h1>Alan AI News Application</h1>
+            <div className={classes.logoContainer}>
+                <img src={imgSrc} className={classes.alanLogo} alt='alan logo'/>
+            </div>
             <NewsCards articles={newsArticles} />
         </div>
     )

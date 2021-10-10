1492dcefff5eefd2325837b616939011452885bd
pradhanpobitra <pobitra.pradhan.pp@gmail.com>
2021-10-11 02:18:16 +0530

    10-10-2021

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,31 +1,83 @@
-import React from 'react'
-import { Card,CardActions, CardActionArea, CardContent,CardMedia,Button,Typography } from '@material-ui/core'
-import classNames from 'classnames'
-import useStyles from './styles'
+import React, { useState, useEffect, useRef } from "react";
+import {
+  Card,
+  CardActions,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import classNames from "classnames";
+import useStyles from "./styles";
 
-const NewsCard = ({ article: { description,publishedAt,source,title,url,urlToImage } , i, activeArticle }) => {
-    
-    const classes = useStyles()
+const NewsCard = ({
+  article: { description, publishedAt, source, title, url, urlToImage },
+  i,
+  activeArticle,
+}) => {
+  const classes = useStyles();
+  // const [elRefs,setElRefs] = useState([])
+  const ref = useRef(null);
+  const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop - 50);
 
-    return (
-      <Card className={classNames(classes.card, activeArticle === i ? classes.activeCard : null)}>
-          <CardActionArea href={url} target='_blank'>
-              <CardMedia className={classes.media} image={urlToImage || 'https://media4.s-nbcnews.com/j/newscms/2019_01/2705191/nbc-social-default_b6fa4fef0d31ca7e8bc7ff6d117ca9f4.nbcnews-fp-1200-630.png'}/>
-              <div className={classes.details}>
-                  <Typography variant='body2' color='textSecondary' component='h2'>{(new Date(publishedAt)).toDateString()}</Typography>
-                  <Typography variant='body2' color='textSecondary' component='h2'>{source.name}</Typography>
-              </div>
-              <Typography className={classes.title} gutterBottom variant='h5'>{title}</Typography>
-              <CardContent>
-                  <Typography variant='body2' color='textSecondary' component='p'>{description}</Typography>
-              </CardContent>
-          </CardActionArea>
-          <CardActions className={classes.cardActions}>
-              <Button size='small' color='primary'>Learn More</Button>
-              <Typography variant='h5' color='textSecondary'>{i+1}</Typography>
-          </CardActions>
-      </Card>
-    )
-}
+  // useEffect(() => {
+  //     setElRefs(refs => Array(20).fill().map((_,j) => refs[j] || createRef()));
+  // }, [])
 
-export default NewsCard
+  // useEffect(() => {
+  //     if(i === activeArticle && elRefs[activeArticle]) {
+  //         scrollToRef(elRefs[activeArticle]);
+  //     }
+  // }, [i, activeArticle, elRefs])
+
+  useEffect(() => {
+    if (i === activeArticle) scrollToRef(ref);
+  }, [i, activeArticle]);
+
+  return (
+    <Card
+      ref={ref}
+      className={classNames(
+        classes.card,
+        activeArticle === i ? classes.activeCard : null
+      )}
+    >
+      <CardActionArea href={url} target="_blank">
+        <CardMedia
+          className={classes.media}
+          image={
+            urlToImage ||
+            "https://media4.s-nbcnews.com/j/newscms/2019_01/2705191/nbc-social-default_b6fa4fef0d31ca7e8bc7ff6d117ca9f4.nbcnews-fp-1200-630.png"
+          }
+        />
+        <div className={classes.details}>
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {new Date(publishedAt).toDateString()}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {source.name}
+          </Typography>
+        </div>
+        <Typography className={classes.title} gutterBottom variant="h5">
+          {title}
+        </Typography>
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+        <Typography variant="h5" color="textSecondary">
+          {i + 1}
+        </Typography>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default NewsCard;
